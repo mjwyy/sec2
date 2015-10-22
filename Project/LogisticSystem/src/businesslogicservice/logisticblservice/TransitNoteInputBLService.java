f491c808f00356988ef5d96190dc95838d859694
王梦麟 <kylin@wangmenglindeMacBook-Pro.local>
2015-10-22 19:06:09 +0800

    10.22注释修改

--- Project/LogisticSystem/src/businesslogicservice/logisticblservice/TransitNoteInputBLService.java
+++ Project/LogisticSystem/src/businesslogicservice/logisticblservice/TransitNoteInputBLService.java
@@ -6,17 +6,15 @@ import vo.TransitNoteOnTransitVO;
 public interface TransitNoteInputBLService extends NoteInputBLService {
 
 	/**
-	 * 前置条件：中转中心业务员要求输入中转中心中转单
-	 * 后置条件：系统检查单据信息的格式，反馈检查结果
-	 * 
+	 * 中转中心业务员输入中转中心中转单信息
+	 *
 	 * @param centerTransitDocVO 中转中心中转单VO
 	 * @return 单据信息格式检查结果
 	 */
 	public ResultMsg inputCenterTransitDoc(TransitNoteOnTransitVO centerTransitDocVO);
 	
 	/**
-	 * 前置条件：中转中心业务员核对信息后要求提交单据
-	 * 后置条件：系统提示单据信息录入成功，并提交单据给总经理审批
+	 * 中转中心业务员核对信息后要求提交单据
 	 * 
 	 * @param centerTransitDocVO 中转中心中转单VO
 	 * @return 

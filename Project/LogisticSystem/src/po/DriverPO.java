621d2f9ee88c18551fc523c5637e0b2efc27f7ad
xriver <xjh14@software.nju.edu.cn>
2015-10-24 10:44:58 +0800

    替换了新版DriverPO

--- Project/LogisticSystem/src/po/DriverPO.java
+++ Project/LogisticSystem/src/po/DriverPO.java
@@ -2,79 +2,96 @@ package po;
 
 import java.io.Serializable;
 
+/**
+ * 表示司机信息的PO
+ * @author River
+ *
+ */
 public class DriverPO implements Serializable {
 
 	private static final long serialVersionUID = 5894748451667870139L;
 	
-	String driverNumber = null;
-	String hallNumber = null;
-	String name = null;
-	String birthday = null;
-	String IDCardNumber = null;
-	String phoneNumber = null;
-	String gender = null;
-	String licenseDate = null;
 	
 	public DriverPO(String driverNumber, String hallNumber, String name,
 			String birthday, String iDCardNumber, String phoneNumber,
-			String gender, String licenseDate){
+			String gender, String licenseDate) {
+		super();
 		this.driverNumber = driverNumber;
 		this.hallNumber = hallNumber;
 		this.name = name;
 		this.birthday = birthday;
-		this.IDCardNumber = iDCardNumber;
+		IDCardNumber = iDCardNumber;
 		this.phoneNumber = phoneNumber;
 		this.gender = gender;
 		this.licenseDate = licenseDate;
 	}
 	
+	/**
+	 * 司机编号
+	 */
+	String driverNumber = null;
+	
+	/**
+	 * 营业厅编号
+	 */
+	String hallNumber = null;
+	
+	/**
+	 * 司机姓名
+	 */
+	String name = null;
+	
+	/**
+	 * 司机生日
+	 */
+	String birthday = null;
+	
+	/**
+	 * 司机身份证号
+	 */
+	String IDCardNumber = null;
+	
+	/**
+	 * 司机电话号码
+	 */
+	String phoneNumber = null;
+	
+	/**
+	 * 司机性别：“Male”，“Female”
+	 */
+	String gender = null;
+	
+	/**
+	 * 驾照过期时间
+	 */
+	String licenseDate = null;
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	public String getDriverNumber() {
 		return driverNumber;
-	}
-	public void setDriverNumber(String driverNumber) {
-		this.driverNumber = driverNumber;
 	}
 	public String getHallNumber() {
 		return hallNumber;
 	}
-	public void setHallNumber(String hallNumber) {
-		this.hallNumber = hallNumber;
-	}
 	public String getName() {
 		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
 	}
 	public String getBirthday() {
 		return birthday;
 	}
-	public void setBirthday(String birthday) {
-		this.birthday = birthday;
-	}
 	public String getIDCardNumber() {
 		return IDCardNumber;
-	}
-	public void setIDCardNumber(String iDCardNumber) {
-		IDCardNumber = iDCardNumber;
 	}
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
 	public String getGender() {
 		return gender;
 	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
 	public String getLicenseDate() {
 		return licenseDate;
-	}
-	public void setLicenseDate(String licenseDate) {
-		this.licenseDate = licenseDate;
 	}
 	
 }

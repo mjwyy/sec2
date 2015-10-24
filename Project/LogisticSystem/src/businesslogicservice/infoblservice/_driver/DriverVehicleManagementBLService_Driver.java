d3fd5ab1206515546aa4e2e5259c0b511cf8e359
xriver <xjh14@software.nju.edu.cn>
2015-10-24 22:20:53 +0800

    去除遗留的乱码

--- Project/LogisticSystem/src/businesslogicservice/infoblservice/_driver/DriverVehicleManagementBLService_Driver.java
+++ Project/LogisticSystem/src/businesslogicservice/infoblservice/_driver/DriverVehicleManagementBLService_Driver.java
@@ -10,10 +10,10 @@ public class DriverVehicleManagementBLService_Driver{
 	
 
 	public void drive(DriverVehicleManagementBLService service){
-		service.addDriver(new DriverVO("00210", "����", "2001-10-04", null, null, null, null));
-		service.deleteDriver(new DriverVO("00210", "����", "2001-10-04", null, null, null, null));
-		service.findDriver(new DriverVO("00210", "����", "2001-10-04", null, null, null, null));
-		service.modifyDriver(new DriverVO("00210", "����", "2001-10-04", null, null, null, null));
+		service.addDriver(new DriverVO("00210", "Mark", "2001-10-04", null, null, null, null));
+		service.deleteDriver(new DriverVO("00210", "Mark", "2001-10-04", null, null, null, null));
+		service.findDriver(new DriverVO("00210", "Mark", "2001-10-04", null, null, null, null));
+		service.modifyDriver(new DriverVO("00210", "Mark", "2001-10-04", null, null, null, null));
 		service.addVehicle(new VehicleInfoVO("00120", "21221", null,new File("vehicle.txt")));
 		service.deleteVehicle(new VehicleInfoVO("00120", "21221", null,new File("vehicle.yxy")));
 		service.findVehicle(new VehicleInfoVO("00120", "21221", null,new File("vehilce.txt")));

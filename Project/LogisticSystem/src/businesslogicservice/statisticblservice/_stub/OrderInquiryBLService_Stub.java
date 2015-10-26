88e5ddaab55915467966e8def080ed6fc0217510
王梦麟 <kylin@wangmenglindeMacBook-Pro.local>
2015-10-26 19:20:54 +0800

    19:20不漏任何接口了

--- Project/LogisticSystem/src/businesslogicservice/statisticblservice/_stub/OrderInquiryBLService_Stub.java
+++ Project/LogisticSystem/src/businesslogicservice/statisticblservice/_stub/OrderInquiryBLService_Stub.java
@@ -1,5 +1,21 @@
 package businesslogicservice.statisticblservice._stub;
 
-public class OrderInquiryBLService_Stub {
+import businesslogic.util.ResultMsg;
+import businesslogicservice.statisticblservice.OrderInquiryBLService;
+import vo.NoteHistoryVO;
+
+public class OrderInquiryBLService_Stub implements OrderInquiryBLService {
+
+	@Override
+	public ResultMsg inputBarcode(String inputBarcode) {
+		System.out.println("inputBarcode succeed");
+		return new ResultMsg(true,null);
+	}
+
+	@Override
+	public NoteHistoryVO submitBarcode(String inputBarcode) {
+		System.out.println("submitBarcode succeed");
+		return new NoteHistoryVO(null,null);
+	}
 
 }
--- Project/LogisticSystem/src/presentation/commodityui/StorageInquiryPartUI.java
+++ Project/LogisticSystem/src/presentation/commodityui/StorageInquiryPartUI.java
@@ -1,5 +1,13 @@
 package presentation.commodityui;
 
+import businesslogicservice.commodityblservice.StorageInquiryPartBLService;
+
 public class StorageInquiryPartUI {
 
+	private StorageInquiryPartBLService service;
+
+	public void init(StorageInquiryPartBLService service) {
+		this.service = service;
+	}
+	
 }

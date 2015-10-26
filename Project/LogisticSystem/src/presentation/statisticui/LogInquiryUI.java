1629c389b9607efe88632dd5e8d3292933a0d0af
xriver <xjh14@software.nju.edu.cn>
2015-10-26 15:47:42 +0800

    上传财务人员相关UI

---
+++ Project/LogisticSystem/src/presentation/statisticui/LogInquiryUI.java
@@ -0,0 +1,26 @@
+package presentation.statisticui;
+
+import java.util.ArrayList;
+
+import businesslogic.util.ResultMsg;
+import businesslogicservice.statisticblservice.LogInquiryBLService;
+import vo.SystemLogVO;
+
+/**
+ * 查询系统日志的逻辑接口
+ * 
+ * @author River
+ *
+ */
+
+/**
+ * 前置条件：用户选择查询系统日志的功能
+ * 后置条件：显示此界面
+ */
+public class LogInquiryUI {
+	
+	public LogInquiryUI(LogInquiryBLService logic){
+		
+	}
+	
+}

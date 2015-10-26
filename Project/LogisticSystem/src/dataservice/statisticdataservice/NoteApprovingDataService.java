838a68193743666d55c6e869a0d8887479717782
xriver <xjh14@software.nju.edu.cn>
2015-10-26 20:32:29 +0800

    上传遗漏的单据审批DataService

--- Project/LogisticSystem/src/dataservice/statisticdataservice/NoteApprovingDataService.java
+++ Project/LogisticSystem/src/dataservice/statisticdataservice/NoteApprovingDataService.java
@@ -1,5 +1,52 @@
 package dataservice.statisticdataservice;
 
-public interface NoteApprovingDataService {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 
+import dataservice.exception.ElementNotFoundException;
+import po.NotePO;
+import businesslogic.util.DocType;
+
+/**
+ * 单据审批DataService
+ * @author River
+ *
+ */
+public interface NoteApprovingDataService extends Remote{
+	
+	/**
+	 * 获得所有未审批NotePO
+	 *
+	 * @return
+	 */
+	public ArrayList<NotePO> getAllDoc()
+			throws RemoteException;
+	
+	/**
+	 * 根据类型获取NotePO
+	 *
+	 * @param type
+	 * @return
+	 */
+	public ArrayList<NotePO> getDocByType(DocType type)
+			throws RemoteException;
+	
+	/**
+	 * 搜索docPO并通过之
+	 *
+	 * @param docPO
+	 * @return true for successful pass operation
+	 */
+	public boolean passDoc(NotePO docPO)
+			throws RemoteException, ElementNotFoundException;
+	
+	/**
+	 * 搜索docPO并且否决之
+	 *
+	 * @param docPO
+	 * @return true for successful decline operation
+	 */
+	public boolean failDoc(NotePO docPO, String comment)
+			throws RemoteException, ElementNotFoundException;
 }

7869844d2abaa63bc5f00b86805febf46882cb53
kylin1 <18795853969@163.com>
2015-11-10 23:02:58 +0800

    再次调整目录结构
    调整目录结构

---
+++ Project/ExpressSystem/server/src/main/java/data/TransitNoteInputData.java
@@ -0,0 +1,37 @@
+package data;
+
+import dataservice.logisticdataservice.TransitNoteInputDataService;
+import po.TransitNotePO;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+/**
+ * Created by kylin on 15/11/10.
+ */
+public class TransitNoteInputData implements TransitNoteInputDataService {
+    @Override
+    public boolean insert(TransitNotePO po) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(TransitNotePO po) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean update(TransitNotePO po) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public ArrayList<TransitNotePO> find(TransitNotePO po) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<TransitNotePO> findAll() throws RemoteException {
+        return null;
+    }
+}

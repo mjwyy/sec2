4ea0ea0c7668388d64c2d5dd187c4081ae5a8767
iDimple <wqy14@software.nju.edu.cn>
2015-12-17 11:16:43 +0800

    git非要我提交

--- Project/ExpressSystem/common/src/main/java/connection/config/RMIConfig.java
+++ Project/ExpressSystem/common/src/main/java/connection/config/RMIConfig.java
@@ -31,7 +31,7 @@ public class RMIConfig {
 //            }
 //            serverIP = params.get("serverIP");
 //            port =  params.get("port");
-        serverIP = "localhost";
+        serverIP = "114.212.42.84";
         port = "1099";
     }
 

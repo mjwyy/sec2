669f9b869dff16b9dea8512330e4be5b05a39f36
Summer222 <wwz14@software.nju.edu.cn>
2015-12-25 21:21:53 +0800

    serverFrame关掉，server服务不停止
    serverFrame关掉，server服务不停止

--- Project/ExpressSystem/server/src/main/java/connection/config/ServerFrame.java
+++ Project/ExpressSystem/server/src/main/java/connection/config/ServerFrame.java
@@ -42,7 +42,7 @@ public class ServerFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public ServerFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 400);
 		contentPane = new JPanel(){
 			public void paintComponent(Graphics g) {
